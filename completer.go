ab222dc95aca9e2b7c2e1198af7113ee8400b355
Mariusz Obajtek <mariusz.obajtek@futuramo.com>
2018-06-01 13:51:40 +0200

    Lot's of overall improvements

--- completer.go
+++ completer.go
@@ -1,139 +1,148 @@
-package main
+package structprompt
 
 import (
-    "fmt"
-    "reflect"
-    "strings"
+	"fmt"
+	"reflect"
+	"strings"
 
 	"github.com/c-bata/go-prompt"
 )
 
-func parse(s string) {
-/*
-    defer func() {
-        if r := recover(); r != nil {
-            suggestions = []prompt.Suggest{}
-        }
-    }()
-*/
-    showStructArgumentPrompt = false
+type completer struct {
+	i interface{}
 
-    var token Token
-    var tokenValue string
-    
-    var methodToCall reflect.Value
-    var methodToCallType reflect.Type
+	suggestions               []prompt.Suggest
+	structArgumentSuggestions []prompt.Suggest
 
-    argumentCount := 0
-
-    currentValue = reflect.ValueOf(&m).Elem()
-    getFields(currentValue)
-    fields = []string{}
-
-    lexer := NewLexer(s)
-
-    for {
-        token = lexer.NextToken()
-        tokenValue = strings.TrimSpace(token.Value)
-
-        if token.Type == TOKEN_EOF {
-            break
-        }
-
-        switch token.Type {
-        case TOKEN_DOT:
-            currentValue = reflect.Indirect(currentValue.FieldByName(currentField))
-            fields = append(fields, currentField)
-            getFields(currentValue)
-        case TOKEN_PATH_ELEMENT:
-            currentField = tokenValue
-        case TOKEN_METHOD:
-            methodToCall = currentValue.MethodByName(tokenValue)
-            methodToCallType = methodToCall.Type()
-        case TOKEN_COMMA:
-            if !showStructArgumentPrompt {
-                argumentCount += 1
-            }
-        case TOKEN_METHOD_ARGUMENT:
-        case TOKEN_FIELD:
-        case TOKEN_FIELD_VALUE:
-        case TOKEN_LEFT_CURLY_BRACKET:
-            showStructArgumentPrompt = true
-            structArgumentPrompt(methodToCallType.In(argumentCount))
-        case TOKEN_RIGHT_CURLY_BRACKET:            
-        }
-    }
+	fields []string
 }
 
-func completer(t prompt.Document) []prompt.Suggest {
-    parse(t.TextBeforeCursor())
+func (c *completer) complete(t prompt.Document) []prompt.Suggest {
+	defer func() {
+		if r := recover(); r != nil {
+			c.suggestions = []prompt.Suggest{}
+		}
+	}()
 
-    if !showStructArgumentPrompt {
-        p := strings.Split(t.TextBeforeCursor(), "(")
-    	return prompt.FilterHasPrefix(suggestions, p[0], true)
-    } else {
-        return suggestions
-    }
+	var token Token
+	var tokenValue string
+
+	var currentField string
+	var methodToCall reflect.Value
+	var methodToCallType reflect.Type
+
+	currentValue := reflect.ValueOf(c.i)
+	c.getFields(currentValue)
+	c.fields = []string{}
+
+	argumentCount := 0
+	showStructArgumentPrompt := false
+
+	lexer := NewLexer(t.TextBeforeCursor())
+
+	for {
+		token = lexer.NextToken()
+		tokenValue = strings.TrimSpace(token.Value)
+
+		if token.Type == TOKEN_EOF {
+			break
+		}
+
+		switch token.Type {
+		case TOKEN_DOT:
+			currentValue = reflect.Indirect(currentValue.FieldByName(currentField))
+			if currentValue.Kind() == reflect.Interface {
+				currentValue = currentValue.Elem()
+			}
+			c.fields = append(c.fields, currentField)
+			c.getFields(currentValue)
+		case TOKEN_COMMA:
+			if !showStructArgumentPrompt {
+				argumentCount += 1
+			}
+		case TOKEN_PATH_ELEMENT:
+			currentField = tokenValue
+		case TOKEN_METHOD:
+			methodToCall = currentValue.MethodByName(tokenValue)
+			methodToCallType = methodToCall.Type()
+		case TOKEN_LEFT_CURLY_BRACKET:
+			c.structArgumentPrompt(methodToCallType.In(argumentCount))
+			showStructArgumentPrompt = true
+		case TOKEN_RIGHT_CURLY_BRACKET:
+			showStructArgumentPrompt = false
+			argumentCount += 1
+		}
+	}
+
+	c.fields = []string{}
+	p := strings.Split(t.TextBeforeCursor(), "(")
+
+	if showStructArgumentPrompt {
+		return c.structArgumentSuggestions
+	}
+	return prompt.FilterHasPrefix(c.suggestions, p[0], true)
 }
 
-// TODO: encapsulate
-var suggestions []prompt.Suggest
-var currentValue reflect.Value
-var currentField string
-var fields []string
-var m Modules
-var showStructArgumentPrompt bool
+func (c *completer) structArgumentPrompt(arg reflect.Type) {
+	var name string
 
-func structArgumentPrompt(arg reflect.Type) {
-    fmt.Println(arg.NumField())
-    suggestions = make([]prompt.Suggest, arg.NumField())
-    for i := 0; i < arg.NumField(); i++ {
-        suggestions[i] = prompt.Suggest{
-            Text: arg.Field(i).Name,
-            Description: arg.Field(i).Type.String(),
-        }
-    }
+	c.structArgumentSuggestions = []prompt.Suggest{}
+	for i := 0; i < arg.NumField(); i++ {
+		name = arg.Field(i).Name
+		if strings.ToUpper(name[0:1]) == name[0:1] {
+			c.structArgumentSuggestions = append(c.structArgumentSuggestions, prompt.Suggest{
+				Text:        arg.Field(i).Name,
+				Description: arg.Field(i).Type.String(),
+			})
+		}
+	}
 }
 
-func getFields(s reflect.Value) {
-    suggestions = make([]prompt.Suggest, s.NumField() + s.NumMethod())
+func (c *completer) getFields(s reflect.Value) {
+	c.suggestions = []prompt.Suggest{}
+	var name string
 
-    typeOfT := s.Type()
-    prefix := strings.Join(fields, ".")
-    if len(prefix) > 0 {
-        prefix += "."
-    }
-    for i := 0; i < s.NumField(); i++ {
-        suggestions[i] = prompt.Suggest{
-            Text: prefix + typeOfT.Field(i).Name,
-/*            Description: typeOfT.Field(i).Name,*/
-        }
-    }
+	typeOfT := s.Type()
+	prefix := strings.Join(c.fields, ".")
+	if len(prefix) > 0 {
+		prefix += "."
+	}
+	for i := 0; i < s.NumField(); i++ {
+		name = typeOfT.Field(i).Name
+		if strings.ToUpper(name[0:1]) == name[0:1] {
+			c.suggestions = append(c.suggestions, prompt.Suggest{
+				Text: prefix + name,
+			})
+		}
+	}
 
-    for i := 0; i < s.NumMethod(); i++ {
-        suggestions[s.NumField() + i] = prompt.Suggest{
-            Text: prefix + typeOfT.Method(i).Name + "()",
-            Description: methodSignature(typeOfT.Method(i).Type),
-        }
-    }
+	for i := 0; i < s.NumMethod(); i++ {
+		name = typeOfT.Method(i).Name
+		if strings.ToUpper(name[0:1]) == name[0:1] {
+			c.suggestions = append(c.suggestions, prompt.Suggest{
+				Text:        prefix + typeOfT.Method(i).Name + "()",
+				Description: methodSignature(typeOfT.Method(i).Type),
+			})
+		}
+	}
 }
 
 func methodSignature(m reflect.Type) string {
-    arguments := make([]string, m.NumIn())
-    returnTypes := make([]string, m.NumOut())
-    for i := 0; i < m.NumIn(); i++ {
-        arguments[i] = m.In(i).Name()
-    }
-    for i := 0; i < m.NumOut(); i++ {
-        returnTypes[i] = m.Out(i).Name()
-    }
+	arguments := make([]string, m.NumIn())
+	returnTypes := make([]string, m.NumOut())
+	for i := 0; i < m.NumIn(); i++ {
+		arguments[i] = m.In(i).Name()
+	}
+	for i := 0; i < m.NumOut(); i++ {
+		returnTypes[i] = m.Out(i).Name()
+	}
 
-    switch(len(returnTypes)) {
-    case 0 :
-        return fmt.Sprintf("(%s)", strings.Join(arguments[1:], ", "))
-    case 1:
-        return fmt.Sprintf("(%s) %s", strings.Join(arguments[1:], ", "), returnTypes[0])
-    default:
-        return fmt.Sprintf("(%s) (%s)", strings.Join(arguments[1:] , ", "), strings.Join(returnTypes, ", "))
-    } 
+	switch len(returnTypes) {
+	case 0:
+		return fmt.Sprintf("(%s)", strings.Join(arguments[1:], ", "))
+	case 1:
+		return fmt.Sprintf("(%s) %s", strings.Join(arguments[1:], ", "), returnTypes[0])
+	default:
+		return fmt.Sprintf("(%s) (%s)", strings.Join(arguments[1:], ", "), strings.Join(returnTypes, ", "))
+	}
 }
--- executor.go
+++ executor.go
@@ -1,105 +1,127 @@
-package main
+package structprompt
 
 import (
-    "fmt"
-    "reflect"
-    "strconv"
-    "strings"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"git.modulus.eu/go/common/types/uuid"
 )
 
-func executor(s string) {
-/*    defer func() {
-        if r := recover(); r != nil {
-            fmt.Println("Failed to call method")
-        }
-    }()
-*/
-    currentValue := reflect.ValueOf(&m).Elem()
-    currentPathField := ""
-    
-    var methodToCall reflect.Value
-    var methodToCallType reflect.Type
-    var arguments []reflect.Value
-    var currentArgumentKind reflect.Kind
-    
-    var currentStructArgument reflect.Value
-    var currentStructArgumentType reflect.Type
-    var currentStructArgumentFieldKind reflect.Kind
-    var currentStructArgumentField string
-
-    var token Token
-    var tokenValue string
-
-    lexer := NewLexer(s)
-
-    for {
-        token = lexer.NextToken()
-        tokenValue = strings.TrimSpace(token.Value)
-
-        if token.Type == TOKEN_EOF {
-            break
-        }
-
-        switch token.Type {
-        case TOKEN_DOT:
-            currentValue = reflect.Indirect(currentValue.FieldByName(currentPathField))
-        case TOKEN_PATH_ELEMENT:
-            currentPathField = tokenValue
-        case TOKEN_METHOD:
-            methodToCall = currentValue.MethodByName(tokenValue)
-            methodToCallType = methodToCall.Type()
-        case TOKEN_METHOD_ARGUMENT:
-            currentArgumentKind = methodToCallType.In(len(arguments)).Kind()
-            arguments = append(arguments, convertArgument(currentArgumentKind, tokenValue))
-        case TOKEN_LEFT_CURLY_BRACKET:
-            currentStructArgumentType = methodToCallType.In(len(arguments))
-            currentStructArgument = reflect.New(currentStructArgumentType).Elem()
-        case TOKEN_FIELD:
-            currentStructArgumentField = tokenValue
-            currentStructArgumentFieldKind = currentStructArgument.FieldByName(tokenValue).Kind()
-        case TOKEN_FIELD_VALUE:
-            currentStructArgument.FieldByName(currentStructArgumentField).Set(
-                convertArgument(currentStructArgumentFieldKind, tokenValue),
-            )
-        case TOKEN_RIGHT_CURLY_BRACKET:
-            arguments = append(arguments, currentStructArgument) 
-        }
-    }
-
-    callMethod(methodToCall, arguments)
+type executor struct {
+	i interface{}
 }
 
-func convertArgument(kind reflect.Kind, s string) reflect.Value {
-    switch kind {
-    case reflect.Int:
-        res, err := strconv.Atoi(s)
-        if err != nil {
-            panic("DSADS")
-        }
-        return reflect.ValueOf(res)
-    // ...
+func (e *executor) execute(s string) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Failed to call method: ", r)
+		}
+	}()
 
-    default:
-        return reflect.ValueOf(s)
-    }
+	currentValue := reflect.ValueOf(e.i)
+	currentPathField := ""
+	structArgument := false
+
+	var methodToCall reflect.Value
+	var methodToCallType reflect.Type
+	var arguments []reflect.Value
+	var currentArgumentType reflect.Type
+
+	var currentStructArgument reflect.Value
+	var currentStructArgumentType reflect.Type
+	var currentStructArgumentFieldType reflect.Type
+	var currentStructArgumentField string
+
+	var token Token
+	var tokenValue string
+
+	lexer := NewLexer(s)
+
+	for {
+		token = lexer.NextToken()
+		tokenValue = strings.TrimSpace(token.Value)
+
+		if token.Type == TOKEN_EOF {
+			break
+		}
+
+		switch token.Type {
+		case TOKEN_DOT:
+			currentValue = reflect.Indirect(currentValue.FieldByName(currentPathField))
+		case TOKEN_PATH_ELEMENT:
+			currentPathField = tokenValue
+		case TOKEN_METHOD:
+			methodToCall = currentValue.MethodByName(tokenValue)
+			methodToCallType = methodToCall.Type()
+		case TOKEN_METHOD_ARGUMENT:
+			if !structArgument {
+				currentArgumentType = methodToCallType.In(len(arguments))
+				arguments = append(arguments, convertArgument(currentArgumentType, tokenValue))
+			} else {
+				structArgument = false
+			}
+		case TOKEN_LEFT_CURLY_BRACKET:
+			currentStructArgumentType = methodToCallType.In(len(arguments))
+			currentStructArgument = reflect.New(currentStructArgumentType).Elem()
+		case TOKEN_FIELD:
+			currentStructArgumentField = tokenValue
+			currentStructArgumentFieldType = currentStructArgument.FieldByName(tokenValue).Type()
+		case TOKEN_FIELD_VALUE:
+			currentStructArgument.FieldByName(currentStructArgumentField).Set(
+				convertArgument(currentStructArgumentFieldType, tokenValue),
+			)
+		case TOKEN_RIGHT_CURLY_BRACKET:
+			structArgument = true
+			arguments = append(arguments, currentStructArgument)
+		}
+	}
+
+	fmt.Println(arguments, len(arguments))
+
+	callMethod(methodToCall, arguments)
+}
+
+func convertArgument(argument reflect.Type, s string) reflect.Value {
+	switch argument.Kind() {
+	case reflect.Int:
+		res, err := strconv.Atoi(s)
+		if err != nil {
+			panic("DSADS")
+		}
+		return reflect.ValueOf(res)
+	// ...
+	case reflect.Struct:
+
+		s = strings.Trim(s, "\"")
+
+		switch argument.Name() {
+		case "UUID":
+			return reflect.ValueOf(uuid.FromString(s))
+		default:
+			return reflect.ValueOf(s)
+		}
+	default:
+		return reflect.ValueOf(s)
+	}
 }
 
 func callMethod(methodToCall reflect.Value, arguments []reflect.Value) {
-    ret := methodToCall.Call(arguments)
+	ret := methodToCall.Call(arguments)
 
-    for _, v := range ret {
-        switch v.Kind() {
-        case reflect.Slice:
-            fmt.Println("\t[")
-            for i := 0; i < v.Len(); i++ {
-                fmt.Printf("\t\t%+v\n", v.Index(i))
-            }
-            fmt.Println("\t]")
-        default:
-            fmt.Printf("\t%+v\n", v.Interface())
-        }
+	for _, v := range ret {
+		switch v.Kind() {
+		case reflect.Slice:
+			fmt.Println("\t[")
+			for i := 0; i < v.Len(); i++ {
+				fmt.Printf("\t\t%+v\n", v.Index(i))
+			}
+			fmt.Println("\t]")
+		default:
+			fmt.Printf("\t%+v\n", v.Interface())
+		}
 
-
-    }
-    fmt.Println()
+	}
+	fmt.Println()
 }
